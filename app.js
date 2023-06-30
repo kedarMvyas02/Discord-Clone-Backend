85d7b9aef382a0b18350145997581e60d13123b4
Kedar Vyas <kedarvyas02@gmail.com>
2023-06-30 17:24:43 +0530

    changed cors configurement for deployement

--- app.js
+++ app.js
@@ -19,9 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "http://127.0.0.1:3000",
-      "http://localhost:3001",
-      "http://127.0.0.1:3001",
+      "https://discord-clone-frontend-teal.vercel.app/",
     ],
     credentials: true,
   })
