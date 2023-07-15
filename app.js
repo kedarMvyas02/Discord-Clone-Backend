64130534bb2f13af11f0b6fa7c1af6696482097b
Kedar Vyas <kedforstudy02@gmail.com>
2023-07-15 14:03:17 +0530

    cors issue solved

--- app.js
+++ app.js
@@ -16,26 +16,38 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: [
+      "https://discord-clone-frontend-teal.vercel.app",
+      "https://discord-clone-kedarmvyas02.vercel.app",
+      "https://discord-clone-hf2915jx9-kedarmvyas02.vercel.app",
       "http://localhost:3000",
       "http://localhost:3001",
-      "https://discord-clone-frontend-teal.vercel.app/",
     ],
     credentials: true,
   })
 );
+app.options("*", cors());
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://discord-clone-frontend-teal.vercel.app"
-  );
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://discord-clone-kedarmvyas02.vercel.app/"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://discord-clone-hf2915jx9-kedarmvyas02.vercel.app/"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://discord-clone-frontend-teal.vercel.app"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   next();
+// });
+
 dbConnect();
 
 app.use("/users", userRoute);
